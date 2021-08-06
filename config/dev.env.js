654899cafaf7eb4e8fbae80a74eaa1fc74c709cf
孙锋锋 <sunfengfeng@agree.com.cn>
2021-08-06 20:17:49 +0800

    sdfsdf

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   // BASE_API: '"https://api-dev"'
-  BASE_API: '"http://www.sunfengfeng.com"'
+  BASE_API: '"https://www.sunfengfeng.com"'
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://www.sunfengfeng.com"'
+  BASE_API: '"https://www.sunfengfeng.com"'
 }
